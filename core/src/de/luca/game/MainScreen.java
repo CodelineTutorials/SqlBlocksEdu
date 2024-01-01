bbe4afa9d8bba56071da910d316351cb15f92737
lucamaxi <informatiktutorials632@gmail.com>
2024-01-01 01:13:01 +0100

    Partly commented UiPart class. IMPORTANT: FINISH THIS!

--- core/src/de/luca/game/MainScreen.java
+++ core/src/de/luca/game/MainScreen.java
@@ -17,6 +17,8 @@ public class MainScreen implements Screen {
 
     SelectButton button;
     SelectButton button2;
+    SelectButton button3;
+    SelectButton button4;
     SelectButtonGroup buttonGroup;
     Button testButton;
 
@@ -24,9 +26,10 @@ public class MainScreen implements Screen {
         UiManager.setBackground(new Texture("winter_pixabay.jpg"));
         button = new SelectButton("Apfel", 80, 50, 300, 100, "Obstesser");
         button2 = new SelectButton("Tomate", 80, 200, 300, 100, "GemueseTyp");
-        buttonGroup = new SelectButtonGroup(1, 1, button, button2);
+        button3 = new SelectButton("Kirsche", 600, 50, 300, 100, "Kirschenvieh");
+        button4 = new SelectButton("DasIstEinSchoenLangerTestUmDasMalZuPruefen!!!", 600, 200, 300, 100, "LangerText");
+        buttonGroup = new SelectButtonGroup(1, 1, button, button2, button3, button4);
         testButton = new Button("finish", 500, 500, 50, 50);
-
     }
 
     @Override
@@ -38,7 +41,7 @@ public class MainScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0.4f, 0.4f, 0.3f, 1);
         UiManager.refresh();
-        UiManager.draw(button, button2, buttonGroup, testButton);
+        UiManager.draw(button, button2, button3, button4, buttonGroup, testButton);
     }
 
     @Override
--- core/src/de/luca/ui/UiPart.java
+++ core/src/de/luca/ui/UiPart.java
@@ -3,35 +3,86 @@ package de.luca.ui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 
+/**
+ * Abstract class used as base for every displayable UiPart object
+ * @since 1.0
+ */
 public abstract class UiPart {
 
+    /**
+     * x position of UiPart object
+     * @since 1.0
+     */
     private int x;
+
+    /**
+     * y position of UiPart object
+     * @since 1.0
+     */
     private int y;
+
+    /**
+     * Width of UiPart object - 0 by default
+     * @since 1.0
+     */
     private int width = 0; // Value should be changed in the constructor of the subclasses
+
+    /**
+     * Height of UiPart object - 0 by default
+     * @since 1.0
+     */
     private int height = 0; // Value should be changed in the constructor of the subclasses
 
+    /**
+     * Listener object which is used to set events for each UiObject individually
+     * @since 1.0
+     */
     Listener listener = new Listener() {
 
+        /**
+         * Method which is being executed when mouse hovers over UiPart object
+         * @since 1.0
+         */
         @Override
         public void onHover() {
 
         }
 
+        /**
+         * Method which is being executed when mouse is being clicked while hovering over UiPart object
+         * @since 1.0
+         */
         @Override
         public void onClick() {
 
         }
     };
 
+    /**
+     * Constructor base for every child of UiPart
+     * @since 1.0
+     * @param x x position of UiPart object
+     * @param y y position of UiPart object
+     */
     public UiPart(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Setter being used to set events ({@link Listener} for specific UiPart objects
+     * @since 1.0
+     * @param listener Listener which is set for a specific UiPart objects
+     */
     public void setListener(Listener listener) {
         this.listener = listener;
     }
 
+    /**
+     * Getter returning listener applied to a specific UiObject
+     * @since 1.0
+     * @return Listener which handles events of different UiPart objects
+     */
     public Listener getListener() {
         return this.listener;
     }
